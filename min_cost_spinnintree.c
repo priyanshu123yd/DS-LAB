2deaad8b2e08903a9c3807cb5a1f48c1817ae515
priyanshu123yd <114083840+priyanshu123yd@users.noreply.github.com>
2022-12-29 15:45:07 +0530

    Create min_cost_spinnintree.c

---
+++ min_cost_spinnintree.c
@@ -0,0 +1,39 @@
+#include&lt;stdio.h&gt;
+#include&lt;conio.h&gt;
+int a,b,u,v,n,i,j,ne=1;
+int visited[10]= {
+0
+}
+,min,mincost=0,cost[10][10];
+void main() {
+clrscr();
+printf(&quot;\n Enter the number of nodes:&quot;);
+scanf(&quot;%d&quot;,&amp;n);
+printf(&quot;\n Enter the adjacency matrix:\n&quot;);
+for (i=1;i&lt;=n;i++)
+for (j=1;j&lt;=n;j++) {
+scanf(&quot;%d&quot;,&amp;cost[i][j]);
+if(cost[i][j]==0)
+cost[i][j]=999;
+}
+visited[1]=1;
+printf(&quot;\n&quot;);
+while(ne&lt;n) {
+for (i=1,min=999;i&lt;=n;i++)
+for (j=1;j&lt;=n;j++)
+if(cost[i][j]&lt;min)
+if(visited[i]!=0) {
+min=cost[i][j];
+a=u=i;
+b=v=j;
+}
+if(visited[u]==0 || visited[v]==0) {
+printf(&quot;\n Edge %d:(%d %d) cost:%d&quot;,ne++,a,b,min);
+mincost+=min;
+visited[b]=1;
+}
+cost[a][b]=cost[b][a]=999;
+}
+printf(&quot;\n Minimun cost=%d&quot;,mincost);
+getch();
+}
